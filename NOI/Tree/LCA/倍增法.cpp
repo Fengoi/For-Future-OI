c6a2886c55d127b6ca65a0c23a5414524d281c00
Feng <151380765+Fengoi@users.noreply.github.com>
2024-07-24 09:15:08 +0800

    Add

--- NOI/Tree/LCA/倍增法.cpp
+++ NOI/Tree/LCA/倍增法.cpp
@@ -4,21 +4,14 @@ int fa[500005], depth[500005];
 int jump[500005][20];
 vector<int> g[500005];
 void dfs(int u){
-	/*
     for(int v : g[u]){
-        if(v==fa[u]) continue;
+        if(v == fa[u]) continue;
         fa[v] = u;
         depth[v] = depth[u] + 1;
         dfs(v);
     }
-    */
-    for(int i=0;i<g[u].size();i++){
-    	if(g[u][i]==fa[u]) continue;
-    	fa[g[u][i]]=u;
-    	depth[g[u][i]]=depth[u]+1;
-    	dfs(g[u][i]);
-	}
 }
+
 int arbitaryJump(int u, int x){
     for(int k = 0; k <= 19; k++){
         if(x&(1<<k)) u = jump[u][k];
@@ -29,11 +22,9 @@ int arbitaryJump(int u, int x){
 int LCA(int x, int y){
     if(depth[y] > depth[x]) swap(x,y);
     x = arbitaryJump(x, depth[x]-depth[y]);
-    if(x==y){
-//		cout<<x<<"ccc"<<endl;
+    if(x == y){
 		return x;
 	}
-//	cout<<x<<" "<<y<<endl;
     for(int k = 19; k >= 0; k--){
         if(jump[x][k] != jump[y][k]){
             x = jump[x][k];
@@ -56,16 +47,14 @@ int main(){
     dfs(s);
     for(int i = 1; i <= n; i++){
     	jump[i][0] = fa[i];
-//	   	cout<<jump[i][0]<<" ";
 	}
     for(int k = 1; k <= 19; k++)
         for(int i = 1; i <= n; i++){
 			jump[i][k] = jump[ jump[i][k-1] ][k-1];
-//			cout<<jump[i][k]<<endl;
 		}
-    for(int i=1;i<=m;i++){
-    	int a,b;
-    	cin>>a>>b;
-    	cout<<LCA(a,b)<<endl;
+    for(int i = 1; i <= m; i++){
+    	int a, b;
+    	cin >> a >> b;
+    	cout << LCA(a, b) << endl;
 	}
 }
