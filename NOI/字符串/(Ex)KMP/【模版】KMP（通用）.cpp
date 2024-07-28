7d023cb3830033bae9906f9f099716d6c182ba39
Feng <151380765+Fengoi@users.noreply.github.com>
2024-07-28 13:01:01 +0800

    Add

---
+++ NOI/字符串/(Ex)KMP/【模版】KMP（通用）.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long lt;
+const lt N = 1e6 + 10;
+lt pi[N];
+lt la, lb, j;
+char a[N], b[N];
+
+int main(){
+    cin >> a+1;
+    cin >> b+1;
+    la = strlen(a+1), lb = strlen(b+1);
+    for(int i = 2; i <= lb; i++){
+        while(j && b[i] != b[j+1]) 
+            j = pi[j];
+        if(b[j+1] == b[i]) 
+            j++;
+        pi[i] = j;
+    }
+    j = 0;
+    for(int i = 1; i <= la; i++){
+        while(j > 0 && b[j+1] != a[i])
+            j = pi[j];
+        if(b[j+1] == a[i])
+            j++;
+        if(j == lb){
+            printf("%lld\n", i-lb+1);
+            j = pi[j];
+        }
+    }
+    for(int i = 1; i <= lb; i++){
+        printf("%lld ", pi[i]);
+    }
+}
